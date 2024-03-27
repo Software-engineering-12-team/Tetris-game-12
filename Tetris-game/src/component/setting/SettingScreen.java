6cd6c2b1c3ee92621b387b5d159d19249d80971f
eunn25 <rose092504@naver.com>
2024-03-27 20:53:58 +0900

    Remove redundant code

--- Tetris-game/src/component/setting/SettingScreen.java
+++ Tetris-game/src/component/setting/SettingScreen.java
@@ -5,11 +5,6 @@ import javax.swing.*;
 public class SettingScreen extends JFrame {
 
     public SettingScreen() {
-        setTitle("Settings");
-        setSize(400, 550);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-
         SettingElement settingElement = new SettingElement();
         getContentPane().add(settingElement);
 
